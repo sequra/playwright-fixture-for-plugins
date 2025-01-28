855d048494b47306828cd0decb86b5dec7c9dc43
Michel Escalante Álvarez <michel.escalante@sequra.com>
2025-01-28 18:56:51 +0100

    Export base components and utility functions from index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+export { default as Fixture } from './base/Fixture';
+export { default as BackOffice } from './base/BackOffice';
+export { default as SeQuraHelper } from './utils/SeQuraHelper';
+export { default as Page } from './pages/Page';
+export { default as SettingsPage } from './pages/SettingsPage';
+export { default as PaymentMethodsSettingsPage } from './pages/PaymentMethodsSettingsPage';
