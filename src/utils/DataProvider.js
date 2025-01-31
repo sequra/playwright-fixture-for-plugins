a6f429e4ba930cf770893d12494f46aea60d98f3
Michel Escalante Álvarez <michel.escalante@sequra.com>
2025-01-31 18:54:08 +0100

    Add shopper method to DataProvider for flexible checkout form filling

--- src/utils/DataProvider.js
+++ src/utils/DataProvider.js
@@ -29,8 +29,58 @@ export default class DataProvider extends Fixture {
                     { name: 'Portugal', paymentMethods: ['Pagamento Fracionado'] }
                 ];
             default:
-                throw new Error('Invalid merchant reference');
+                throw new Error(`Invalid merchant reference "${merchantRef}"`);
         }
     }
+
+    /**
+     * For filling the checkout form
+     * 
+     * @param {string} alias Use:
+     * - approve: For placing an approved 🍊 order 
+     * - cancel: For placing a cancelled 🍊 order
+     * - nonSpecial: For placing a normal order
+     */
+    shopper(alias = 'nonSpecial') {
+        const shopper = {
+            email: 'test@sequra.es',
+            firstName: 'Fulano',
+            lastName: 'De Tal',
+            address1: "Carrer d'Alí Bei, 7",
+            country: 'ES',
+            city: 'Barcelona',
+            state: 'Barcelona',
+            postcode: '08010',
+            phone: '666666666',
+            dateOfBirth: '01/01/2000',
+            dni: '23232323T',
+            creditCard: {
+                number: '4716773077339777',
+                exp: '12/30',
+                cvc: '123',
+            },
+            otp: ['6', '6', '6', '6', '6']
+        };
+
+        switch (alias) {
+            case 'approve':
+                return {
+                    ...shopper,
+                    firstName: 'Review Test Approve',
+                    lastName: 'Review Test Approve',
+                };
+            case 'cancel':
+                return {
+                    ...shopper,
+                    firstName: 'Review Test Cancel',
+                    lastName: 'Review Test Cancel',
+                };
+            case 'nonSpecial':
+                return shopper;
+            default:
+                throw new Error(`Invalid merchant reference "${alias}"`);
+        }
+    }
+
 
 }
