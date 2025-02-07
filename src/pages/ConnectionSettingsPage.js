9777c8b2578cb07d8cdc16e7f7417a7110e74a7c
Michel Escalante Álvarez <michel.escalante@sequra.com>
2025-02-07 12:30:34 +0100

    Add ConnectionSettingsPage and update index.js to export it

---
+++ src/pages/ConnectionSettingsPage.js
@@ -0,0 +1,70 @@
+import SettingsPage from './SettingsPage.js';
+
+export default class ConnectionSettingsPage extends SettingsPage {
+
+    /**
+    * @param {import('@playwright/test').Page} page
+    * @param {string} baseURL
+    * @param {import('@playwright/test').Expect} expect
+    * @param {import('@playwright/test').Request} request
+    * @param {import('../base/BackOffice.js')} backOffice
+    * @param {import('../utils/SeQuraHelper.js')} helper
+    * @param {string} pageHash Page hash to navigate to
+    */
+    constructor(page, baseURL, expect, request, backOffice, helper) {
+        super(page, baseURL, expect, request, backOffice, helper, 'settings-connection');
+    }
+
+    /**
+    * Init the locators with the locators available
+    * 
+    * @returns {Object}
+    */
+    initLocators() {
+        return {
+            ...super.initLocators(),
+            modalConfirmButton: () => this.page.locator('#sq-modal .sq-button.sqt--primary'),
+            disconnect: () => this.page.getByRole('button', { name: 'Disconnect' }),
+            sandboxOption: (locate = 'input') => this.page.locator(locate === 'input' ? '[type="radio"][value="sandbox"]' : '.sq-radio-input:has([type="radio"][value="sandbox"])'),
+            username: () => this.page.locator('[name="username-input"]'),
+            password: () => this.page.locator('[name="password-input"]'),
+        };
+    }
+
+    async confirmModal() {
+        await this.locators.modalConfirmButton().waitFor({ state: 'visible' });
+        await this.locators.modalConfirmButton().click();
+    }
+
+    async disconnect() {
+        await this.locators.disconnect().click();
+        await this.confirmModal();
+        await this.page.waitForURL(/#onboarding-connect/);
+    }
+
+    /**
+     * Fill the connect form
+     * @param {Object} options 
+     * @param {string} options.username The username to use
+     * @param {string} options.password The password to use
+     */
+    async fillForm(options) {
+        const { sandboxOption, username, password } = this.locators;
+        await sandboxOption('label').click();
+        await username().fill(options.username);
+        await password().fill(options.password);
+    }
+
+    /**
+     * Expect the form to have the values
+     * @param {Object} options 
+     * @param {string} options.username The username to use
+     * @param {string} options.password The password to use
+     */
+    async expectFormToHaveValues(options) {
+        const { username, password, env } = options;
+        await this.expect(this.locators.username(), `Username field should be "${username}`).toHaveValue(username);
+        await this.expect(this.locators.password(), `Password field should be "${password}`).toHaveValue(password);
+        await this.expectToBeChecked(this.locators.sandboxOption(), 'Sandbox environment', true);
+    }
+}
